b2eced514ccc7eb9c3f5cd9b9f369a06ef325629
aibabus <aibek.nazaraliev003005@gmail.com>
2023-12-27 23:37:56 +0100

    asdffffasd

--- src/main/java/com/shop/ShopApplication/config/SecurityConfig.java
+++ src/main/java/com/shop/ShopApplication/config/SecurityConfig.java
@@ -28,8 +28,6 @@ public class SecurityConfig {
                         authorizeRequests
                                 .antMatchers("/api/client/**")
                                 .hasAuthority("CLIENT")
-                                .antMatchers("/api/filial/admin/**")
-                                .hasAuthority("ADMIN")
                                 .antMatchers("/api/admin/saveAdmin","/api/auth/**", "/swagger-ui/**","/swagger-ui.html","/swagger-ui/","/v3/api-docs", "/v3/api-docs/swagger-config", "/api/admin/log", "/api/client/auth/*","/api/admin/*").permitAll()
                                 .anyRequest().authenticated()
                 )
